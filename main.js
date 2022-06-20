e7ec626fb40a9c29d7239206950698b1875005e4
flix2612 <53663200+flix2612@users.noreply.github.com>
2022-06-20 10:22:55 +0200

    Solution L3

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.de/search?q=socks&source=hp&ei=AS6wYravJtmOxc8PwJKngA0&iflsig=AJiK0e8AAAAAYrA8EYiLj5qSiTgVOFlnjhDHb0V78Rjk&ved=0ahUKEwj2s4HQzLv4AhVZR_EDHUDJCdAQ4dUDCAk&uact=5&oq=socks&gs_lcp=Cgdnd3Mtd2l6EAMyCAgAEIAEELEDMgUIABCABDIFCAAQgAQyCwguEIAEEMcBEK8BMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOhQILhCABBCxAxCDARDHARDRAxDUAjoLCAAQgAQQsQMQgwE6CAguELEDEIMBOggIABCxAxCDAToRCC4QgAQQsQMQgwEQxwEQ0QM6CwguELEDEIMBENQCOgsILhCABBDHARCjAjoFCC4QgAQ6CwguEIAEELEDEIMBUABY7gRgnwZoAHAAeACAAWCIAZgDkgEBNZgBAKABAQ&sclient=gws-wiz'
         }
     }
 })
